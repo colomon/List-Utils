33a4dc448bdc0a6e9f45844e908c217fc1ebc2c6
Solomon Foster <colomon@gmail.com>
2010-09-03 11:35:27 -0400

    Add permute and take-while with test files.  Only very very lightly tested at this point!

--- lib/List/Utils.pm
+++ lib/List/Utils.pm
@@ -13,3 +13,40 @@ sub sliding-window(@a, $n) is export(:DEFAULT) {
         }
     }
 }
+
+sub permute(@items) is export(:DEFAULT) {
+    sub pattern_to_permutation(@pattern, @items1) {
+        my @items = @items1;
+        my @r;
+        for @pattern {
+            push @r, @items.splice($_, 1);
+        }
+        @r;
+    }
+
+    sub n_to_pat($n is copy, $length) {
+        my @odometer;
+        for 1 .. $length -> $i {
+            unshift @odometer, $n % $i;
+            $n div= $i;
+        }
+        return $n ?? () !! @odometer;
+    }
+    
+    my $n = 0;
+    gather loop {
+        my @pattern = n_to_pat($n++, +@items);
+        last unless ?@pattern;
+        take pattern_to_permutation(@pattern, @items).item;
+    }
+}
+
+sub take-while(@a, Mu $test) is export(:DEFAULT) {
+    gather {
+        for @a.list {
+            when $test { take $_ }
+            last;
+        }
+    }
+}
+
---
+++ t/03-take-while.t
@@ -0,0 +1,9 @@
+use Test;
+use List::Utils;
+
+plan *;
+
+is take-while((1...*), * <= 10), ~(1...10), "take-while works on a basic infinite loop";
+is take-while((1...*), * <= -1), "", "take-while works if condition is initially false";
+
+done_testing;
