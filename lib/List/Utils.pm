03379f900ec22a6ebef07c9a8fc0666787ae375b
Moritz Lenz <moritz@faui2k3.org>
2011-08-25 14:07:55 +0200

    fix sliding-window-wrapped

--- lib/List/Utils.pm
+++ lib/List/Utils.pm
@@ -11,10 +11,10 @@ sub sliding-window(@a is copy, $n) is export {
 }
 
 sub sliding-window-wrapped(@a, $n) is export(:DEFAULT) {
-    my $a-list = @a.iterator.list;
+    my @a-list = @a;
     my @values;
     gather {
-        while defined(my $a = $a-list.shift) {
+        while defined(my $a = @a-list.shift) {
             @values.push($a);
             @values.shift if +@values > $n;
             take @values if +@values == $n;
